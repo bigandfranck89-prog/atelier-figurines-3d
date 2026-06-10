9233e9abaf89d8419e54cd61c41d9fda6634ebda
bigandfranck89-prog <bigandfranck89@gmail.com>
2026-06-10 16:04:40 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,9 @@
-const C="atelier-v1";
-const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.svg"];
-self.addEventListener("install",e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
-self.addEventListener("activate",e=>{self.clients.claim();});
-self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
+const C="atelier-v3";
+self.addEventListener("install",e=>{self.skipWaiting();});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener("fetch",e=>{
+  e.respondWith(
+    fetch(e.request).then(r=>{const c=r.clone();caches.open(C).then(ca=>ca.put(e.request,c));return r;})
+                    .catch(()=>caches.match(e.request))
+  );
+});
